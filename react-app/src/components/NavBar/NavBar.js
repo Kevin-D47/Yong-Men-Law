a0f86144723704c44b1ad5b1e90ab145b9d530c4
Kevin Duong <kevduong47@gmail.com>
2023-08-21 20:01:05 -0400

    made changes to nav bar

--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 import logoPlaceholder from '../../images/logo-placeholder.png';
+import chineseIcon from '../../images/chinese-icon.png';
 
 import './NavBar.css'
 
@@ -10,22 +11,22 @@ const NavBar = () => {
     <nav>
       <div className='nav-container'>
         <div className='nav-contact-info'>
-          <div>Contact Us:</div>
+          <NavLink className='nav-contact-us-link' to='/contact'>Contact Us:</NavLink>
           <div>(770) 936-3991</div>
           <div>|</div>
           <div>yongmen@yongmenlaw.com</div>
         </div>
         <div className='nav-wrapper'>
-          <div>
-            <NavLink to='/' className='logo-container'>
-              <img className='nav-logo' src={logoPlaceholder} />
-              <div className='logo-text-container'>
-                <div>LAW OFFICE</div>
-                <div>OF</div>
-                <div>Yong Men</div>
-              </div>
-            </NavLink>
-          </div>
+
+          <NavLink to='/' className='logo-container'>
+            <img className='nav-logo' src={logoPlaceholder} />
+            <div className='logo-text-container'>
+              <div>LAW OFFICE</div>
+              <div>OF</div>
+              <div>Yong Men</div>
+            </div>
+          </NavLink>
+
           <div className='nav-pages-bttn-container'>
             <NavLink to='/attorney' className='nav-attorney-link'>
               ATTORNEY
@@ -40,9 +41,12 @@ const NavBar = () => {
               CONTACT US
             </NavLink>
           </div>
+
           <div className='chinese-trans-bttn'>
-            中文版
+            <img className='chinese-icon' src={chineseIcon} />
+            <div className='chinese-txt'>中文版</div>
           </div>
+
         </div>
       </div>
     </nav>
