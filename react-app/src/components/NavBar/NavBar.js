9a5bd69d9b0f26b9d1e4d79381bbab03d23c03c1
Kevin Duong <kevduong47@gmail.com>
2023-08-24 17:00:22 -0400

    added chinese navbar

--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -7,6 +7,7 @@ import chineseIcon from '../../images/chinese-icon.png';
 import './NavBar.css'
 
 const NavBar = () => {
+
   return (
     <nav>
       <div className='nav-container'>
@@ -42,13 +43,54 @@ const NavBar = () => {
             </NavLink>
           </div>
 
-          <div className='chinese-trans-bttn'>
+          <button className='chinese-trans-bttn'>
             <img className='chinese-icon' src={chineseIcon} />
             <div className='chinese-txt'>中文版</div>
-          </div>
+          </button>
 
         </div>
       </div>
+
+
+      {/* <div className='nav-container'>
+        <div className='nav-contact-info'>
+          <NavLink className='nav-contact-us-link' to='/contact'>联系我们:</NavLink>
+          <div>(770) 936-3991</div>
+          <div>|</div>
+          <div>yongmen@yongmenlaw.com</div>
+        </div>
+        <div className='nav-wrapper'>
+
+          <NavLink to='/' className='logo-container'>
+            <img className='nav-logo' src={logoPlaceholder} />
+            <div className='logo-text-container'>
+              <div>永门律师事务所</div>
+            </div>
+          </NavLink>
+
+          <div className='nav-pages-bttn-container'>
+            <NavLink to='/attorney' className='nav-attorney-link'>
+              律师
+            </NavLink>
+            <NavLink to='/practice' className='nav-attorney-link'>
+              法律实践
+            </NavLink>
+            <NavLink to='/resources' className='nav-attorney-link'>
+              资源
+            </NavLink>
+            <NavLink to='/contact' className='nav-attorney-link'>
+              联系我们
+            </NavLink>
+          </div>
+
+          <button className='chinese-trans-bttn'>
+            <img className='chinese-icon' src={chineseIcon} />
+            <div className='chinese-txt'>English</div>
+          </button>
+
+        </div>
+      </div> */}
+
     </nav>
   );
 }
